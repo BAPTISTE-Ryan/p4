80b9f603ac29de6914a873501c31d7c0fc389144
ryani <ryani@Raj.mshome.net>
2019-07-24 23:08:15 +0200

    noc

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -29,7 +29,7 @@ public class EcritureComptableTest {
                 vLibelle, vDebit, vCredit);
         return vRetour;
     }
-
+ 
     @Test
     public void isEquilibree() {
         EcritureComptable vEcriture;
